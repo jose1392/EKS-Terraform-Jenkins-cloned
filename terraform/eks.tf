e7999cc0eacb797ddd573697931c609de6f2363d
jose1392 <150645133+jose1392@users.noreply.github.com>
2024-04-24 18:55:16 -0400

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -17,6 +17,30 @@ locals {
     Example = local.name
   }
 }
+
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = "~> 4.0"
+
+  name = local.name
+  cidr = local.vpc_cidr
+
+  azs             = local.azs
+  private_subnets = local.private_subnets
+  public_subnets  = local.public_subnets
+  intra_subnets   = local.intra_subnets
+
+  enable_nat_gateway = true
+
+  public_subnet_tags = {
+    "kubernetes.io/role/elb" = 1
+  }
+
+  private_subnet_tags = {
+    "kubernetes.io/role/internal-elb" = 1
+  }
+}
+
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "19.15.1"
