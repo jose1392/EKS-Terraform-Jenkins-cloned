6221fdf3ac4459afe895b7de8f3b8bbc10e74592
jose1392 <150645133+jose1392@users.noreply.github.com>
2024-04-25 11:35:29 -0400

    Update eks.tf

--- terraform/eks.tf
+++ terraform/eks.tf
@@ -58,14 +58,12 @@ resource "aws_security_group" "eks_sg" {
     protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
   egress {
     from_port = 0
     to_port   = 0
     protocol = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
-}
 
 # Outputs
 output "cluster_name" {
